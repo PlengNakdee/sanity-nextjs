a330ed3faa2d899b5b142099cbdbb9b9d11bfd14
PlengNakdee <plengwn@gmail.com>
2021-03-25 04:12:18 +0000

    delete files

--- .projenrc.js
+++ .projenrc.js
@@ -11,9 +11,13 @@ const project = new web.NextJsTypeScriptProject({
     'postcss@^7', 
     'autoprefixer@^9',
     '@sanity/cli',
-    '@sanity/core',
     'concurrently',
     'next-sanity',
+    'next',
+    'react',
+    'react-dom',
+    'react-router',
+    'react-router-dom',
   ],
   
   devDeps: [
@@ -27,22 +31,22 @@ const project = new web.NextJsTypeScriptProject({
 // build.exec('yarn build:frontend');
 // build.exec('yarn build:backend');
 
-const devFrontend = project.addTask('dev:frontend', {
-    description: 'Dev Next.js & Sanity',
-    category: tasks.TaskCategory.BUILD,
-});
-devFrontend.exec('concurrently \"next dev\" \"cd studio && sanity start\"');
+// const devFrontend = project.addTask('dev:frontend', {
+//     description: 'Dev Next.js & Sanity',
+//     category: tasks.TaskCategory.BUILD,
+// });
+// devFrontend.exec('concurrently \"next dev\" \"cd studio && sanity start\"');
 
-const buildFrontend = project.addTask('build:frontend', {
-    description: 'Build Next.js & Sanity',
-    category: tasks.TaskCategory.BUILD,
-});
-buildFrontend.exec('echo "Building Sanity to public/studio…" && cd studio && sanity build ../public/studio -y && cd .. && cross-env NODE_ENV=production next build');
+// const buildFrontend = project.addTask('build:frontend', {
+//     description: 'Build Next.js & Sanity',
+//     category: tasks.TaskCategory.BUILD,
+// });
+// buildFrontend.exec('echo "Building Sanity to public/studio…" && cd studio && sanity build ../public/studio -y && cd .. && cross-env NODE_ENV=production next build');
 
-const startNext = project.addTask('start:next', {
-    description: 'Start Next.js',
-    category: tasks.TaskCategory.BUILD,
-});
-startNext.exec('next start');
+// const startNext = project.addTask('start:next', {
+//     description: 'Start Next.js',
+//     category: tasks.TaskCategory.BUILD,
+// });
+// startNext.exec('next start');
 
 project.synth();
