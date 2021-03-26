203bffad0ae827ffdbee12c858c433e18092c02d
PlengNakdee <plengwn@gmail.com>
2021-03-26 04:48:30 +0000

    add sanity schema

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -11,6 +11,33 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    {
+      name: 'blog',
+      type: 'document',
+      title: 'Blog',
+      fields: [
+        {
+          title: "Title",
+          name: "title",
+          type: "string"
+        },
+        {
+          title: "Category",
+          name: "Category",
+          type: "string"
+        },
+        {
+          title: 'Slug',
+          name: 'slug',
+          type: 'slug'
+        },
+        {
+          title: 'Text',
+          name: 'text',
+          type: 'array',
+          of: [{ type: 'block' }]
+        }
+      ],
+    }
   ]),
 })
