546e236ba01a995b7c89d9a45bc7a8a5077db6e1
PlengNakdee <plengwn@gmail.com>
2021-03-26 06:51:15 +0000

    components breakdown

---
+++ pages/components/BlogContent.tsx
@@ -0,0 +1,55 @@
+
+const BlogContent = () => {
+  return (
+    <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+      <div className="flex-shrink-0">
+        <img className="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixqx=NvgVnYOcQe&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt="" />
+      </div>
+      <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+        <div className="flex-1">
+          <p className="text-sm font-medium text-indigo-600">
+            <a href="#" className="hover:underline">
+              Video
+              </a>
+          </p>
+          <a href="#" className="block mt-2">
+            <p className="text-xl font-semibold text-gray-900">
+              How to use search engine optimization to drive sales
+              </p>
+            <p className="mt-3 text-base text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.
+              </p>
+          </a>
+        </div>
+        <div className="mt-6 flex items-center">
+          <div className="flex-shrink-0">
+            <a href="#">
+              <span className="sr-only">Brenna Goyette</span>
+              <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixqx=NvgVnYOcQe&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+            </a>
+          </div>
+          <div className="ml-3">
+            <p className="text-sm font-medium text-gray-900">
+              <a href="#" className="hover:underline">
+                Brenna Goyette
+                </a>
+            </p>
+            <div className="flex space-x-1 text-sm text-gray-500">
+              <time dateTime="2020-03-10">
+                Mar 10, 2020
+                </time>
+              <span aria-hidden="true">
+                &middot;
+                </span>
+              <span>
+                4 min read
+                </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BlogContent;
