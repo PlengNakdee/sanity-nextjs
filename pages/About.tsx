15862923ef38ddf871b2eefe260acc7732d68be8
PlengNakdee <plengwn@gmail.com>
2021-03-30 04:31:35 +0000

    add about page

--- pages/About.tsx
+++ pages/About.tsx
@@ -1,11 +1,11 @@
-export default function BlogPage() {
+export default function About() {
   return (
     <div className="bg-white overflow-hidden">
       <div className="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
         <div className="hidden lg:block bg-gray-50 absolute top-0 bottom-0 left-3/4 w-screen"></div>
         <div className="mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none">
           <div>
-            <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Case Study</h2>
+            <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">About</h2>
             <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Meet Whitney</h3>
           </div>
         </div>
