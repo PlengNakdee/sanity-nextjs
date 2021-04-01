e65ede8380b538e018ef4bab8c07e1f8331d1c9a
PlengNakdee <plengwn@gmail.com>
2021-04-01 04:48:04 +0000

    add admin page

---
+++ pages/Admin.tsx
@@ -0,0 +1,143 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+
+const Admin = () => {
+  return (
+    <div>
+      <Header />
+      <div className="relative pt-10 px-4 sm:px-6 lg:px-8">
+        <div>
+          <h3 className="text-lg leading-6 font-medium text-gray-900">
+            Applicant Information
+  </h3>
+          <p className="mt-1 max-w-2xl text-sm text-gray-500">
+            Personal details and application.
+  </p>
+        </div>
+        <div className="mt-5 border-t border-gray-200">
+          <dl className="divide-y divide-gray-200">
+            <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
+              <dt className="text-sm font-medium text-gray-500">
+                Full name
+      </dt>
+              <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                <span className="flex-grow">Margot Foster</span>
+                <span className="ml-4 flex-shrink-0">
+                  <button type="button" className="bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Update
+          </button>
+                </span>
+              </dd>
+            </div>
+            <div className="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
+              <dt className="text-sm font-medium text-gray-500">
+                Application for
+      </dt>
+              <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                <span className="flex-grow">Backend Developer</span>
+                <span className="ml-4 flex-shrink-0">
+                  <button type="button" className="bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Update
+          </button>
+                </span>
+              </dd>
+            </div>
+            <div className="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
+              <dt className="text-sm font-medium text-gray-500">
+                Email address
+      </dt>
+              <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                <span className="flex-grow">margotfoster@example.com</span>
+                <span className="ml-4 flex-shrink-0">
+                  <button type="button" className="bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Update
+          </button>
+                </span>
+              </dd>
+            </div>
+            <div className="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
+              <dt className="text-sm font-medium text-gray-500">
+                Salary expectation
+      </dt>
+              <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                <span className="flex-grow"> $120,000</span>
+                <span className="ml-4 flex-shrink-0">
+                  <button type="button" className="bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Update
+          </button>
+                </span>
+              </dd>
+            </div>
+            <div className="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
+              <dt className="text-sm font-medium text-gray-500">
+                About
+      </dt>
+              <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                <span className="flex-grow">
+                  Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
+        </span>
+                <span className="ml-4 flex-shrink-0">
+                  <button type="button" className="bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Update
+          </button>
+                </span>
+              </dd>
+            </div>
+            <div className="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
+              <dt className="text-sm font-medium text-gray-500">
+                Attachments
+      </dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                <ul className="border border-gray-200 rounded-md divide-y divide-gray-200">
+                  <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                    <div className="w-0 flex-1 flex items-center">
+
+                      <svg className="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
+                      </svg>
+                      <span className="ml-2 flex-1 w-0 truncate">
+                        resume_back_end_developer.pdf
+              </span>
+                    </div>
+                    <div className="ml-4 flex-shrink-0 flex space-x-4">
+                      <button type="button" className="bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        Update
+              </button>
+                      <span className="text-gray-300" aria-hidden="true">|</span>
+                      <button type="button" className="bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        Remove
+              </button>
+                    </div>
+                  </li>
+                  <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                    <div className="w-0 flex-1 flex items-center">
+
+                      <svg className="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
+                      </svg>
+                      <span className="ml-2 flex-1 w-0 truncate">
+                        coverletter_back_end_developer.pdf
+              </span>
+                    </div>
+                    <div className="ml-4 flex-shrink-0 flex space-x-4">
+                      <button type="button" className="bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        Update
+              </button>
+                      <span className="text-gray-300" aria-hidden="true">|</span>
+                      <button type="button" className="bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        Remove
+              </button>
+                    </div>
+                  </li>
+                </ul>
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Admin;
--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -25,8 +25,6 @@ const Header = () => {
 
             <a href="#" className="font-medium text-gray-500 hover:text-gray-900">Application</a>
 
-            <a href="#" className="font-medium text-gray-500 hover:text-gray-900">Marketplace</a>
-
             <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">Log in</a>
           </div>
         </nav>
@@ -51,7 +49,6 @@ const Header = () => {
 
             <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Application</a>
 
-            <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Marketplace</a>
           </div>
           <a href="#" className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100">
             Log in
