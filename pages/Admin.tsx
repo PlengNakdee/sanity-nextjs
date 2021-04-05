2b7a5c645616435f073155850a06f8c00a203df8
PlengNakdee <plengwn@gmail.com>
2021-04-05 09:03:07 +0000

    checkpoint

--- pages/Admin.tsx
+++ pages/Admin.tsx
@@ -1,35 +1,18 @@
-
-import { useFetchUser } from './lib/user';
-
 const Admin = () => {
-  const { user, loading } = useFetchUser()
 
   return (
     <div>
       <h1>Next.js and Auth0 Example</h1>
+      <>
+        <p>
+          To test the login click in <a href="/api/auth/login"><i>Login</i> </a>
+        </p>
+        <p>
+          Once you have logged in you should be able to click in{' '}
+          <a href="/profile"><i>Profile</i></a> and <a href="/api/auth/logout"><i>Logout</i></a>
+        </p>
+      </>
 
-      {loading && <p>Loading login info...</p>}
-
-      {!loading && !user && (
-        <>
-          <p>
-            To test the login click in <a href="/api/auth/login"><i>Login</i> </a>
-          </p>
-          <p>
-            Once you have logged in you should be able to click in{' '}
-            <i>Profile</i> and <i>Logout</i>
-          </p>
-        </>
-      )}
-
-      {user && (
-        <>
-          <h4>Rendered user info on the client</h4>
-          <img src={user.picture} alt="user picture" />
-          <p>nickname: {user.nickname}</p>
-          <p>name: {user.name}</p>
-        </>
-      )}
     </div>
   )
 }
