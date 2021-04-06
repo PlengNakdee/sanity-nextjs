9ad4b3a06801bcf6e9966e8d86a4b903fd3d1222
PlengNakdee <plengwn@gmail.com>
2021-04-06 09:08:31 +0000

    checkpoint

--- .projenrc.js
+++ .projenrc.js
@@ -24,46 +24,21 @@ const project = new web.NextJsTypeScriptProject({
     'next-sanity',
     'next-router',
     '@sanity/client',
-    '@sanity/cli',
     'react',
-    'react-dom',
-    'react-router',
-    'react-router-dom',
     'swr',
     '@auth0/nextjs-auth0',
-    '@auth0/auth0-react'
+    'dotenv-keyvault',
   ],
 
   devDeps: [
     'concurrently',
     '@types/react-router',
     '@types/react-router-dom',
+    '@sanity/cli',
+    'azure-cli'
   ],
 
 });
-// const build = project.tasks.tryFind('build');
-// build.reset();
-// build.description = 'Perform a full build of frontend and backend';
-// build.exec('yarn build:frontend');
-// build.exec('yarn build:backend');
-
-// const devFrontend = project.addTask('dev:frontend', {
-//     description: 'Dev Next.js & Sanity',
-//     category: tasks.TaskCategory.BUILD,
-// });
-// devFrontend.exec('concurrently \"next dev\" \"cd studio && sanity start\"');
-
-// const buildFrontend = project.addTask('build:frontend', {
-//     description: 'Build Next.js & Sanity',
-//     category: tasks.TaskCategory.BUILD,
-// });
-// buildFrontend.exec('echo "Building Sanity to public/studio…" && cd studio && sanity build ../public/studio -y && cd .. && cross-env NODE_ENV=production next build');
-
-// const startNext = project.addTask('start:next', {
-//     description: 'Start Next.js',
-//     category: tasks.TaskCategory.BUILD,
-// });
-// startNext.exec('next start');
 
 new JsonFile(project, '.theia/settings.json', {
   obj: {
--- create-dotenv.sh
+++ create-dotenv.sh
@@ -1,5 +1,7 @@
 PORT=3000
 
+# az keyvault secret show --vault-name "auth0" --name "AUTH0-CLIENT-ID" --query "value"
+
 cat <<EOF > .env.local
 AUTH0_BASE_URL=$(gp url $PORT)
 AUTH0_ISSUER_BASE_URL=https://dev-pqtgmoe1.us.auth0.com
