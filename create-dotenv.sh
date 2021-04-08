8e8e4f4eb528ea1811f7c13bae81bc92159a03d4
PlengNakdee <plengwn@gmail.com>
2021-04-08 03:06:52 +0000

    checkpoint

--- create-dotenv.sh
+++ create-dotenv.sh
@@ -1,10 +1,16 @@
 PORT=3000
 
-# az keyvault secret show --vault-name "auth0" --name "AUTH0-CLIENT-ID" --query "value"
+getClientId () {
+az keyvault secret show --vault-name "auth0" --name "AUTH0-CLIENT-ID" --query "value"
+}
+
+getClientSecret() {
+    az keyvault secret show --vault-name "auth0" --name "AUTH0-CLIENT-SECRET" --query "value"
+}
 
 cat <<EOF > .env.local
 AUTH0_BASE_URL=$(gp url $PORT)
 AUTH0_ISSUER_BASE_URL=https://dev-pqtgmoe1.us.auth0.com
-AUTH0_CLIENT_ID=
-AUTH0_CLIENT_SECRET=
+AUTH0_CLIENT_ID=$(getClientId)
+AUTH0_CLIENT_SECRET=$(getClientSecret)
 EOF
